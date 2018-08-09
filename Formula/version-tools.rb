e76e74023f8a582454b066c8ecae427c709143e8
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-08-09 10:07:13 -0600

    Corrected hash.

--- Formula/version-tools.rb
+++ Formula/version-tools.rb
@@ -3,7 +3,7 @@ class VersionTools < Formula
   homepage "https://github.com/smartystreets/version-tools"
   version "0.0.2"
   url "https://github.com/smartystreets/version-tools/archive/#{version}.tar.gz"
-  sha256 "d85cae222c0d68c2551be183dbda60fb9014846cad9d1f037a89e92f6cda8711"
+  sha256 "293b9db2926c6da164b4264767948a5e92b741f9aef5c7f6add3d5b9c4387f2c"
 
   depends_on "git"
 
