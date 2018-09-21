9c9b33b164981222b84a0a6eaf0fab8fb846eae5
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-09-21 11:45:39 -0600

    Latest version tools.

--- Formula/version-tools.rb
+++ Formula/version-tools.rb
@@ -1,11 +1,11 @@
 class VersionTools < Formula
   desc "Simple versioning tools that function without external dependencies."
   homepage "https://github.com/smartystreets/version-tools"
-  version "0.0.5"
+  version "0.0.6"
   url "https://github.com/smartystreets/version-tools/archive/#{version}.tar.gz"
-  sha256 "3ba745e198a18137957319b740ba8957f8a1d337f7bd5e965e8d93e2c6dae3a4"
+  sha256 "68b54d8ed17e3515382db3cdefdf13ace10e76cea14455d65563618b578e4da6"
 
-  # sha256: curl -sL "https://github.com/smartystreets/version-tools/archive/0.0.5.tar.gz" | sha256sum
+  # sha256: curl -sL "https://github.com/smartystreets/version-tools/archive/0.0.6.tar.gz" | sha256sum
 
   depends_on "git"
 
