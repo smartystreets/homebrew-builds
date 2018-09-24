f47159a522206dba16b66e59ea70f97c9b3c1210
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2018-09-24 11:11:13 -0600

    Latest version.

--- Formula/version-tools.rb
+++ Formula/version-tools.rb
@@ -1,11 +1,11 @@
 class VersionTools < Formula
   desc "Simple versioning tools that function without external dependencies."
   homepage "https://github.com/smartystreets/version-tools"
-  version "0.0.6"
+  version "0.0.8"
   url "https://github.com/smartystreets/version-tools/archive/#{version}.tar.gz"
-  sha256 "68b54d8ed17e3515382db3cdefdf13ace10e76cea14455d65563618b578e4da6"
+  sha256 "a6358740f693385797e84a03c47a8111ce50b2417d69b1c8480430e4f3f41103"
 
-  # sha256: curl -sL "https://github.com/smartystreets/version-tools/archive/0.0.6.tar.gz" | sha256sum
+  # sha256: curl -sL "https://github.com/smartystreets/version-tools/archive/0.0.8.tar.gz" | sha256sum
 
   def install
     bin.install "src/tagit"
