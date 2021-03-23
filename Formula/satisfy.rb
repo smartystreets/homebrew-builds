2d59469d1971584e81ad22f2e0daca0ce3f2ab02
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2021-03-23 08:32:27 -0600

    Satisfy v0.9.7.

--- Formula/satisfy.rb
+++ Formula/satisfy.rb
@@ -1,9 +1,9 @@
 class Satisfy < Formula
   desc "Simple ultra lightweight dependency manager."
   homepage "https://github.com/smartystreets/satisfy"
-  version "v0.9.6"
+  version "v0.9.7"
   url "https://github.com/smartystreets/satisfy/releases/download/#{version}/satisfy_darwin-amd64.tar.gz"
-  sha256 "a7bff338c07c35909a164b687a12ec0a528c578f99eb4f50e4f44ea936fe3893"
+  sha256 "f4c9fb3bf752010ef229309c00e4d73cd4313cb9d32860ff54b974e7b5d22e56"
 
   # sha256: curl -sL "https://github.com/smartystreets/satisfy/releases/download/v0.9.5/satisfy_darwin-amd64.tar.gz" | sha256sum
 
@@ -12,3 +12,6 @@ class Satisfy < Formula
   end
 
 end
+
+
+https://github.com/smartystreets/satisfy/releases/download/v0.9.7/satisfy_darwin-amd64.tar.gz
