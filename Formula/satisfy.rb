28dfc204af82c03f129b30be43bc4a9ecf83dd56
Ryan Cox <ryan@smarty.com>
2024-05-06 14:13:01 -0600

    Satisfy v0.12.0

--- Formula/satisfy.rb
+++ Formula/satisfy.rb
@@ -1,12 +1,12 @@
 class Satisfy < Formula
   desc "Simple ultra lightweight dependency manager."
   homepage "https://github.com/smarty/satisfy"
-  version "v0.11.18"
+  version "v0.12.0"
   url "https://github.com/smarty/satisfy/releases/download/#{version}/satisfy_darwin-amd64.tar.gz"
-  sha256 "ab6812fccf1a7c41c1c0aa7ff88a49c4aecc45ed8abe34493f5ad40f684909a4"
+  sha256 "140b6cfdfd880fefaf1c8edabfe9bac2f4decddaba98cfc1fd5ca141453efb11"
 
   # to compute sha256:
-  # curl -sL "https://github.com/smarty/satisfy/releases/download/v0.11.18/satisfy_darwin-amd64.tar.gz" | sha256sum
+  # curl -sL "https://github.com/smarty/satisfy/releases/download/v0.12.0/satisfy_darwin-amd64.tar.gz" | sha256sum
 
   def install
     bin.install "satisfy"
