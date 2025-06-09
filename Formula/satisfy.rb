50696d417581183679a37509df4323089b508084
Ryan Cox <ryan@smarty.com>
2025-06-09 10:08:53 -0600

    Satisfy v0.12.4

--- Formula/satisfy.rb
+++ Formula/satisfy.rb
@@ -1,13 +1,13 @@
 class Satisfy < Formula
   desc "Simple ultra lightweight dependency manager."
   homepage "https://github.com/smarty/satisfy"
-  version "v0.12.0"
+  version "v0.12.4"
   if Hardware::CPU.arm?
     url "https://github.com/smarty/satisfy/releases/download/#{version}/satisfy_darwin-arm64.tar.gz"
-    sha256 "60ba5ee7ffe40af292b3397a2f48797c0aa00a504e1370592ad1130068e56375"
+    sha256 "1340141c73fba996998ba93d2f384b61baa017a777d348cf599ae98464869c3b"
   else
     url "https://github.com/smarty/satisfy/releases/download/#{version}/satisfy_darwin-amd64.tar.gz"
-    sha256 "08d18cc0aad7d76045578dd203577a2564edffe27f655438a1e5e1d9663306cc"
+    sha256 "f97faee9e80435bda677f6326b7b3bd887f68337693cb96c0ad81f54a7e90d55"
   end
 
   # to compute sha256 on intel mac:
